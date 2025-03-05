bc5b703942c419e9e58ce71c6fe4629a1dd2e5f2
berdov <berdov@MacBook-Air-berdov.local>
2025-03-05 13:51:47 +0300

    add a lil reviews

--- lamoda_parsing/lamoda_parsing.py
+++ lamoda_parsing/lamoda_parsing.py
@@ -231,8 +231,8 @@ def parse_catalog(url, n):
         browser.quit()
 
 def main():
-    for i in range(20, 100):
-        parse_catalog(f"https://www.lamoda.ru/c/15/shoes-women/?sitelink=topmenuW&l=4&page={i}", i)
+    for i in range(1, 100):
+        parse_catalog(f"https://www.lamoda.ru/c/647/hats-golovnyeubory/?sitelink=topmenuW&l=3&page={i}", i)
 
 if __name__ == "__main__":
     main()
