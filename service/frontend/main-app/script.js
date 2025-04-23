ca7092d59ac1643219c88ecac4ad0c2563edb3fb
Win4ap <mrwinchap@gmail.com>
2025-04-23 13:50:40 +0300

    Style impr. and raw fuctionality

--- service/frontend/main-app/script.js
+++ service/frontend/main-app/script.js
@@ -5,3 +5,17 @@ document.querySelectorAll('.model-btn').forEach(btn => {
         document.querySelector('.model-info strong').textContent = this.textContent;
     });
 });
+
+document.querySelector('.input-container input').addEventListener('input', function() {
+    const icon = this.nextElementSibling.querySelector('img');
+    icon.src = this.value.trim() ? 'svgs/pointer.svg' : 'svgs/upload.svg';
+});
+
+document.getElementById('submit-button').addEventListener('click', function() {
+    const input = document.getElementById('product-input');
+    if (input.value.trim() === '') {
+        window.location.href = 'https://example.com';
+        return;
+    }    
+    console.log('Отправка данных:', input.value);
+});
